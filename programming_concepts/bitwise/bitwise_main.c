b745b22a0fe44ffa70b4033046dcef363090ad70
M0hanrajp <mohanrajp.github@gmail.com>
2024-10-31 22:15:39 +0530

    Added integer to binary conversion program

--- programming_concepts/bitwise/bitwise_main.c
+++ programming_concepts/bitwise/bitwise_main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
+// function declaration
+void printBinaryBits(unsigned int userInput);
+
 int main(void) {
-    int a = 20; // 1100
-    printf("%d\n", a >> 4);
+    int userInput = 0;
+    printf("Enter a number to display it's binary format: ");
+    scanf("%d", &userInput);
+    printBinaryBits(userInput);
     return 0;
-    // 10 - 1010, 6 - 0110, 1100 - 12
+}
+
+// function definiton
+void printBinaryBits(unsigned int userInput) {
+    for(int i = 31; i >= 0; i--) {
+        // Left shift by i bits
+        int andMask = 1 << i;
+        // checkBitsSet will have 0 or 1 based on the result
+        int checkBitsSet = userInput & andMask;
+        checkBitsSet == 0 ? printf("0") : printf("1");
+        if(i % 4 == 0)
+            printf(" ");
+    }
+    printf("\n");
 }
