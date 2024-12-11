0993306a150bff033564027386222d9c60179760
M0hanrajp <mohanrajp.github@gmail.com>
2024-12-11 13:53:41 +0530

    8KYU, codewars solution

---
+++ codewars/8KYU/l1_set_alarm.c
@@ -0,0 +1,10 @@
+/* The function should return true if you are employed and not on vacation 
+ * (because these are the circumstances under which you need to set an alarm). 
+ * It should return false otherwise. Examples:
+
+employed | vacation 
+true     | true     => false
+true     | false    => true
+false    | true     => false
+false    | false    => false */
+
