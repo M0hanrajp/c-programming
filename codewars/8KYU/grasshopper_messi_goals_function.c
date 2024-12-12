efa9e1ce4ab3a989c6dac5702ba124cbffaf4a4a
M0hanrajp <mohanrajp.github@gmail.com>
2024-12-12 18:22:35 +0530

    8KYU, codewars solution

---
+++ codewars/8KYU/grasshopper_messi_goals_function.c
@@ -0,0 +1,8 @@
+/* Complete the function to return his total number of goals in all three leagues.
+Note: the input will always be valid.
+
+For example:
+5, 10, 2  -->  17 */
+int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
