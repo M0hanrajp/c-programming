e4746f19e5514068ba8ff9cd77c3708f15c87c9b
M0hanrajp <mohanrajp.github@gmail.com>
2024-12-15 22:17:51 +0530

    8KYU, codewars solution

---
+++ codewars/8KYU/get_character_from_ascii_value.c
@@ -0,0 +1,8 @@
+/* Write a function which takes a number and returns the corresponding ASCII char for that value.
+Example:
+65 --> 'A'
+97 --> 'a'
+48 --> '0 */
+char char_of_code (unsigned charcode) {
+  return (char )charcode;
+}
