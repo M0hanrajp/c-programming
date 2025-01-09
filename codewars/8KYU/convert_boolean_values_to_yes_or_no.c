4b97c375e63f8750aa4a8d5af870b6e42053fc66
M0hanrajp <mohanrajp.github@gmail.com>
2025-01-09 17:01:56 +0530

    8KYU, codewars solution

---
+++ codewars/8KYU/convert_boolean_values_to_yes_or_no.c
@@ -0,0 +1,5 @@
+/*/omplete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false. */
+#include <stdbool.h>
+const char *bool_to_word (bool value) {
+  return value == true ? "Yes" : "No";
+}
