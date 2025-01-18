065783ea5f6e1bb3b18758e739f53362169643d4
M0hanrajp <mohanrajp.github@gmail.com>
2025-01-18 22:25:44 +0530

    7KYU, codewars solution

---
+++ codewars/7KYU/Simple Fun #2: Circle of Numbers.c
@@ -0,0 +1,22 @@
+/* Task
+Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighbouring numbers is equal (note that 0 and n - 1 are neighbouring, too).
+Given n and firstNumber/first_number/first-number, find the number which is written in the radially opposite position to firstNumber.
+Example
+For n = 10 and firstNumber = 2, the output should be 7
+
+Input/Output
+[input] integer n
+
+A positive even integer.
+
+Constraints: 4 ≤ n ≤ 1000.
+
+[input] integer firstNumber/first_number/first-number
+
+Constraints: 0 ≤ firstNumber ≤ n - 1
+
+[output] an integer */
+int circle_of_numbers(int n, int first_number) {
+  if(first_number >= n / 2){return first_number - n / 2;}
+  return first_number + n / 2;
+}
