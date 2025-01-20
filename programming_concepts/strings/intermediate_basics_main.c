080092dfa30c980c0afb36a8d2a0051229a80348
M0hanrajp <mohanrajp.github@gmail.com>
2025-01-20 21:48:07 +0530

    Updated code with new information. * Array behavior during assignment of a string literal or char array. * How char pointers are different from char arrays.

--- programming_concepts/strings/intermediate_basics_main.c
+++ programming_concepts/strings/intermediate_basics_main.c
@@ -1,3 +1,4 @@
+// pointers and strings
 #include <stdio.h>
 
 int main(void) {
@@ -6,16 +7,46 @@ int main(void) {
     char copy[20];
     char *newString = NULL;
 
-    // assigning to a new char array
+    // assigning to a new char array using = operator
     copy = word;
-    
-    // trying to assign directly of type word[]
+/*     intermediate_basics_main.c:11:10: error: assignment to expression with array type
+       11 |     copy = word;
+          |          ^ 
+       - array name represents a fixed memory address (a pointer to the first element of the array). 
+       - This memory is allocated during compile time, and you cannot reassign it. */
+
+    // assign string literal to an already allocated word[]
     word = "computer";
+/*     intermediate_basics_main.c:14:10: error: assignment to expression with array type
+       14 |     word = "computer";
+          |
+       - word[] is a statically allocated array, and as such, its memory location cannot be changed 
+       after its declaration.
+       - When you declare char word[] = "something";, the compiler allocates enough memory for "something" 
+       (including the null terminator \0) and initializes the array. However, the array itself is not a 
+       pointer, so you cannot reassign it to another string. */
 
-    // trying to assign directly of type char *
-    words = "Code";
+    // assign new string literal to char *
+    // previously 
+/*  words was assigned "Code Code Code"
+    (gdb) printf"%p\n", words
+    0x555555556004    
+    (gdb) printf"%s\n", words
+    Code Code Code */
+    words = "Keep coding";
+    // Change in address for words after it is assigned new string literal
+/*  (gdb) printf"%p\n", words
+    0x555555556013
+    (gdb) printf"%s\n", words
+    Keep coding */
+    // Since words is a char pointer it can be assigned different address.
 
-    // Assigning to a new char pointer
+    // assign char * to a new char * with string literal
     newString = words;
+    // new string is a char *, hence it can hold any char *'s address.
+/*  (gdb) printf"%p\n", newString
+    0x555555556013
+    (gdb) printf"%s\n", newString
+    Keep coding */
     return 0;
 }
