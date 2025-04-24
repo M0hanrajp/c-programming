61c260bbf34ed50950a4e9cc998bccad04dbb9b3
M0hanrajp <mohanrajp.github@gmail.com>
2025-04-24 22:07:48 +0530

    refactor & learnings(arrays): Searching algorithms * What is linear search * What is binary search * How to perform binary search on unsorted arrays * How to perform binray search on order agnostic arrays

--- programming_concepts/arrays/algorithms/binary_search/main.c
+++ programming_concepts/arrays/algorithms/binary_search/main.c
@@ -1,41 +1,57 @@
-/* A c program to implement binary search */
+/* A c program to implement binary search 
+ * What: For a given sorted array either in descending or ascending
+ * 1. divide the array in two halves
+ * 2. compare the middle element with the target element
+ * 2a. check if target element is greater or smaller than middle elememt
+ * 2b. if target element greater than middle element it will be present on right side of array else left side.
+ * so search for the element but from start index and end index should be modified according to postion of target element.
+ * // TODO: why
+ * Why: In linear search where we are making multiple comparison for each pass.
+ * Time complexity 
+ * */
 #include <stdio.h>
 #include <strings.h>
-// Function declaration for sorting the array in ascending order 
+#include <stdbool.h>
+
+// Function declarations  
 void bubble_sort(int *input_array, int array_size);
-// Function declaration for swap
 void swap(int *variable_a, int *variable_b);
-// Funciton declaration to display array
 void display_array(int *array_element, int array_size);
-// Function declaration for binary search
 int binary_search(int *array, int low, int high, int *find_element);
-int main(void)
-{
-    int array[10] = {2, 5, -3, -4, 6, 7, 9, 0, 8, 1}, search_element = 0, result = 0;
-    printf("Enter any element < 10: ");
-    scanf("%d", &search_element);
-    printf("Initialized array:\n");display_array(array, 10);
-    bubble_sort(array, 10);
-    printf("Initialized array is sorted into ascending order\n");display_array(array, 10);
-    result = binary_search(array, 0, 10, &search_element);
+int orderAgnosticBinarySearch(int *array, int low, int high, int *find_element);
+
+int main(void) {
+
+    int array[] = {-1, -45, 45, 66}, result = 0;
+    int arrSize = sizeof(array)/sizeof(*array);
+
+    int search_element = 45;
+    printf("Initialized array:\n");display_array(array, arrSize);
+    // Case 1: if the array you are changing in the code will be unsorted
+    // bubble_sort(array, 10);
+    // printf("Initialized array is sorted into ascending order\n");display_array(array, 10);
+    // result = binary_search(array, 0, arrSize - 1, &search_element);
+    // Case 2: if array is sorted but you dont know in which order the array is sorted
+    result = orderAgnosticBinarySearch(array, 0, arrSize - 1, &search_element);
     result == -1 ? printf("Element was not found in the provided array!\n")
-                : printf("Element was found in the array at index %d\n", result);
+                 : printf("Element was found in the array at index %d\n", result);
     return 0;
 }
+
 // Function definition for sorting the array
+// When you have been given an unsorted array you can sort it using the below function
+// Note it sorts in ascending order
 void bubble_sort(int *input_array, int array_size)
 {
-    for(int index = 0; index < array_size; index++)
-    {
-        for(int elements = 1; elements < array_size - index; elements++)
-        {
-            if(*(input_array + elements) < *(input_array + elements - 1))
-            {
-                swap(&(*(input_array + elements)), &(*(input_array + elements - 1)));
+    for(int index = 0; index < array_size; index++) {
+        for(int elements = 1; elements < array_size - index; elements++) {
+            if(input_array[elements] < input_array[elements - 1]) {
+                swap(&input_array[elements], &input_array[elements - 1]);
             }
         }
     }
 }
+
 // Function definition for swapping elements
 void swap(int *variable_a, int *variable_b)
 {
@@ -44,34 +60,73 @@ void swap(int *variable_a, int *variable_b)
     *variable_a = *variable_b;
     *variable_b = temp;
 }
+
 // Function definition for printing array elements
 void display_array(int *array_element, int array_size)
 {
-    for(int subscript = 0; subscript < array_size; subscript++)
-    {
-        printf("A[%2d] = %2d & address: %u\n", subscript, *(array_element + subscript), array_element + subscript);
+    for(int subscript = 0; subscript < array_size; subscript++) {
+        printf("A[%2d] = %2d & address: %p\n", subscript, *(array_element + subscript), array_element + subscript);
     }
 }
+
 // Function definition for binary search
+// this is used when the array is sorted in ascending order
 int binary_search(int *array, int low, int high, int *find_element)
 {
-    while(low <= high)
-    {
-        int mid = low + (high - low)/2;
+    while(low <= high) {
+        int mid = low + (high - low)/2; // if the integer is very large
+                                        //
         // Return that the element was found
-        if(*find_element == *(array + mid))
+        if(*find_element == array[mid])
             return mid;
+
         /* if the element value is greater than the middle element of the array 
          * then we need to compare the right half of the array after the middle 
          * element which will help shorten the path to find the element.
          * so here we use low (left indexer of array to move towards the desired search space) */
-        if(*find_element > *(array + mid))
+        if(*find_element > array[mid])
             low = mid + 1;
+
         /* if the element is found to be smaller than the middle element then its
          * found to be at the left side of the array hence we move the high (right indexer
          * to the left side of the search space array ) */
         else
             high = mid - 1;
     }
+    return -1;
+}
+
+// When you do not know the array is sorted in ascending or descending order
+int orderAgnosticBinarySearch(int *array, int low, int high, int *find_element) {
+    
+    /* if the array is in ascending order , array[low] < array[high]
+     * if the array is in descending order, array[low] > array[high]
+     * if array[low] == array[high], then all elements are same
+     */
+    bool isAscending = array[low] < array[high];
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if(array[mid] == *find_element)
+            return mid;
+
+        if(isAscending) {
+            if(*find_element > array[mid])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        } else {
+            // descending order
+            // all elements greater than mid will be to the left
+            // so search to the left else search to the right
+            if(*find_element > array[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+    }
+
     return -1;
 }
--- programming_concepts/arrays/algorithms/linear_search/main.c
+++ programming_concepts/arrays/algorithms/linear_search/main.c
@@ -1,16 +1,27 @@
-/* A C program to learn linear search algorithm */
+/* A C program to learn linear search algorithm 
+ * What: Search the element index by iterating the loop and look for the element
+ * if the element is found, return the index/value or -1 if failed.
+ * Why: to search in increments of 1, linear
+ * */
 
 #include <stdio.h>
 
 // Function declaration - Linear search
-int linearSearchAlgorithm(int *array, int array_size, int *search_element);
+int linearSearchAlgorithmIntegers(int *array, int array_size, int *search_element);
+
+int linearSearchAlgorithmCharacters(char *array, int *search_element);
 
 int main(void) {
 
-    int array[] = {10, 50, 30, 70, 80, 20, 90, 40}, key = 0, result = 0;
+    int array[] = {10, 50, 30, 70, 80, 20, 90, 40}, result = 0;
+    char arr[] = {"Carpe Diem\n"};
+
+    int key = 'e'; // enter the integer/char that needs to be found
     int array_size = sizeof(array)/sizeof(*array);
-    printf("Enter key to be found :: ");scanf("%d", &key);
-    result = linearSearchAlgorithm(array, array_size, &key);
+
+    //result = linearSearchAlgorithm(array, array_size, &key);
+    result = linearSearchAlgorithmCharacters(arr, &key);
+
     if(result != -1) {
         printf("The element was found at %d index in the array\n", result);
     }
@@ -19,15 +30,21 @@ int main(void) {
     return 0;
 }
 
-// Function declaration - Linear search
+// Function definition - Linear search
 int linearSearchAlgorithm(int *array, int array_size, int *search_element) {
     for(int element_index = 0; element_index < array_size; element_index++) {
         if(*search_element == array[element_index]) {
-            return element_index;
-            break;
+            return element_index; // you can return value to using arr[element_index]
         }
     }
     return -1;
 }
 
-
+int linearSearchAlgorithmCharacters(char *array, int *search_element) {
+    for(int element_index = 0; array[element_index] != '\0'; element_index++) {
+        if((char)*search_element == array[element_index]) {
+            return element_index; // you can return value to using arr[element_index]
+        }
+    }
+    return -1;
+}
